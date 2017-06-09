2b53a8ea5686a849d3e4231d829b0e5079e4de6b
xiazdong <xiazdong@126.com>
2017-06-09 09:04:59 +0800

    Demo4：DialogFragment+Lottie使用

---
+++ app/src/main/java/xiazdong/me/fragmentdemo/fragment/ProgressDialogFragment.java
@@ -0,0 +1,27 @@
+package xiazdong.me.fragmentdemo.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import xiazdong.me.fragmentdemo.R;
+
+public class ProgressDialogFragment extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        View root = inflater.inflate(R.layout.fragment_progress_dialog, container);
+        return root;
+    }
+
+    public static ProgressDialogFragment newInstance() {
+        return new ProgressDialogFragment();
+    }
+}
